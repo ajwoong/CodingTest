b2f2d5bddcac70d2fecf13eee84ebb9f73eddb84
ajwoong <sosloahn@gmail.com>
2025-05-16 13:36:57 +0900

    0516

---
+++ test2.py
@@ -0,0 +1,25 @@
+import math
+
+n = int(input())
+m = int(input())
+
+board = [[math.inf for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(m):
+    a, b, c = map(int,input().split())
+    board[a][b] = min(board[a][b], c)
+
+for i in range(1,n+1):
+    board[i][i] = 0
+
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if(board[i][j] > board[i][k] + board[k][j]):
+                board[i][j] = board[i][k] + board[k][j]
+
+
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        print(board[i][j], end= " ")
+    print("")
