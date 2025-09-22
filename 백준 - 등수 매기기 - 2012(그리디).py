d912b82ab14ebc402c010de6c6fb6f7d3a072d88
ajwoong <sosloahn@gmail.com>
2025-09-22 20:40:14 +0900

    0922

---
+++ 백준 - 등수 매기기 - 2012(그리디).py
@@ -0,0 +1,16 @@
+n = int(input())
+blist = []
+
+for i in range(n):
+    b = int(input())
+    blist.append(b)
+
+blist.sort()
+
+answer = 0
+rank = 1
+for b in blist:
+    answer += abs(rank - b)
+    rank+=1
+
+print(answer)
